c28fd713d800173b31db6a3ce76f181276aa992b
BMiller <79605481+bmiller-avg@users.noreply.github.com>
2023-01-20 14:05:27 -0500

    end state C02-01

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
-import logo from "../assets/red30-tech-logo.png";
+import { Routes, Route } from "react-router-dom";
+import Header from "./Header";
+import Home from "./Home";
+import Categories from "./Categories";
+import About from "./About";
+import Category from "./Category";
 
 function App() {
   return (
     <div className="app">
-      <header className="container">
-        <img
-          className="logo"
-          src={logo}
-          alt="Red30 Tech logo"
-          title="Red30 Tech | Home"
-        />
-      </header>
+     
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home title="Welcome to Red30 Tech" />} />
+        <Route path="about" element={<About />} />
+        <Route path="categories" element={<Categories />}>
+          <Route path="category" element={<Category />} />
+        </Route>
+      </Routes>
 
       <footer className="container">
         &copy;2022 | <a href="https://red30tech.com/">Red30 Tech</a>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,23 @@
-import { Link, NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../assets/red30-tech-logo.png";
 
 export default function Header() {
-  const getClass = ({ isActive }) => (isActive ? "nav-active" : null);
-
+  
   return (
     <header className="container">
-      <Link to="/">
-        <img
-          className="logo"
-          src={logo}
-          alt="Red30 Tech logo"
-          title="Red30 Tech | Home"
-        />
-      </Link>
-
-      <nav>
-        <NavLink to="/" className={getClass}>
-          Home
-        </NavLink>
-        <NavLink to="/about" className={getClass}>
-          About
-        </NavLink>
-        <NavLink to="/categories" className={getClass}>
-          Categories
-        </NavLink>
-      </nav>
-    </header>
+    <Link to="/">
+      <img
+        className="logo"
+        src={logo}
+        alt="Red30 Tech logo"
+        title="Red30 Tech | Home"
+      />
+    </Link>
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/categories">Categories</Link>
+    </nav>
+  </header>
   );
 }
